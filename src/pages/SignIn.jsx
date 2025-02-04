b1bf7ef6e5795c62aeea18d4f31e5c98a8bf0076
oniadeoye <emmanueloni164@gmail.com>
2025-02-04 14:12:11 +0100

    added redux toolkit

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,62 +1,71 @@
-import React from 'react'
-import { useState } from 'react';
+import React, { useState } from 'react';
 import OAuth from '../components/OAuth';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
 
 function SignIn() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const dispatch = useDispatch();
+    const { loading, error } = useSelector(state => state.user);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        
-        try {
-            const res = await fetch ('http://localhost:3001/users' )
+        dispatch(signInStart());
 
+        try {
+            const res = await fetch('http://localhost:3001/usersData');
             const users = await res.json();
+            
+            if (!Array.isArray(users)) throw new Error('Invalid response from server');
 
             const user = users.find(user => user.email === email && user.password === password);
 
             if (user) {
                 alert('Sign in successful');
+                dispatch(signInSuccess(user));
             } else {
-                throw new Error('Invalid email or password');
-        }   } catch (error) {
-            console.error('Error signing in:', error);
-            alert('Failed to sign in');
-    }   }
-  
-  return (
-    <div className="form-container">
-      <h2>Sign In</h2>
-      <form onSubmit={handleSubmit}>
-        {/* <div>
-          <label htmlFor="name">UserName</label>
-          <input type="text" placeholder="Enter your name" id="name" />
-        </div> */}
+                dispatch(signInFailure('Invalid email or password'));
+            }
+        } catch (error) {
+            dispatch(signInFailure(error.message));
+        }
+    };
 
-        <div>
-          <label htmlFor="email">Email</label>
-          <input type="text" id="email" placeholder="isioma@gmail.com"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)} />
+    return (
+        <div className="form-container">
+            <h2>Sign In</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="email">Email</label>
+                    <input 
+                        type="text" 
+                        id="email" 
+                        placeholder="isioma@gmail.com"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </div>
+
+                <div>
+                    <label htmlFor="password">Password</label>
+                    <input 
+                        type="password" 
+                        id="password"
+                        value={password} 
+                        autoComplete='current-password'
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+
+                {loading && <p>Loading...</p>}
+                {error && <p style={{ color: 'red' }}>{error}</p>}
+
+                <button type="submit">Sign In</button>
+                <OAuth />
+            </form>
         </div>
-
-        <div>
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password"
-          value={password} autoComplete='current-password'
-          onChange={(e) => setPassword(e.target.value)}/>
-        </div>
-
-       
-
-        <button type="submit">Sign In</button>
-        <OAuth />
-      </form>
-      
-    </div>
-  );
+    );
 }
-    
 
-export default SignIn
+export default SignIn;
