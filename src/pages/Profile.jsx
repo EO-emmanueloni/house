5089930b8d2f81beb9082aaa8abbaca35fdb1abb
oniadeoye <emmanueloni164@gmail.com>
2025-02-05 14:03:14 +0100

    image upload

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,52 @@
 import { useSelector } from "react-redux";
-import { useRef } from "react";
+import { app } from "../firebase";
+import { useRef, useState,useEffect } from "react";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 function Profile() {
   const fileRef = useRef(null);
   const { currentUser } = useSelector(state => state.user);
+  const [file, setFile] = useState(undefined);
+  console.log(file)
+  const [formData, setFormData] = useState({});
+
+  useEffect(() => {
+    if(file) {
+      handleFileUpload(file);
+    }
+  }, [file]);
+
+  const handleFileUpload = async (file) => {
+    const storage = getStorage(app)
+    const fileName = new Date().getTime() + file.name;  
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+
+    uploadTask.on('state_changed', 
+      (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        console.log('Upload is ' + progress + '% done');
+        switch (snapshot.state) {
+          case 'paused':
+            console.log('Upload is paused');
+            break;
+          case 'running':
+            console.log('Upload is running');
+            break;
+        }
+      }, 
+      (error) => {
+        console.log('Error uploading file:', error);
+      }, 
+      () => {
+        console.log('File uploaded successfully');
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setFormData({ ...formData, photoURL: downloadURL });
+          console.log('File available at', downloadURL);
+        });
+      }
+    );
+  }
 
   return (
     <div className='profile-container'>
@@ -12,7 +55,8 @@ function Profile() {
         {/* Hidden file input */}
         <input style={{
           display: 'none'
-        }} type="file" ref={fileRef} hidden accept="image/*" />
+        }} type="file" ref={fileRef} hidden accept="image/*"
+        onChange={(e) =>setFile(e.target.files[0])} />
         
         {/* Clickable image to trigger file input */}
         <img 
